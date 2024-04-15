55274b52d53f5ec53287f12c56b2fdc8bf55acf5
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-04-15 17:50:04 +0530

    Initial Commit

---
+++ MapKitSwiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitSwiftui
+//
+//  Created by Adarsh Ranjan on 15/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitSwiftui/MapKitSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitSwiftuiApp.swift
+//  MapKitSwiftui
+//
+//  Created by Adarsh Ranjan on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
